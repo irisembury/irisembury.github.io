04e1ae06ee0f5f08323a4f5ab89738d5d8090fa8
Iris Embury <irisembury@gmail.com>
2026-08-17 02:38:27 -0400

    working

--- assets/layout.js
+++ assets/layout.js
@@ -530,7 +530,7 @@ function loadBody() {
                 <div><div class="menu-button"><svg viewBox="0 0 24 24" width="28" height="24"><path fill="currentcolor" d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg></div></div>
             </div>
         </nav>
-        <div class="--page-aligner">
+        <div id="aligner">
             <div class="right-panel">
                 <h3>Display:</h3>
                 <div class="switches-area">
@@ -556,7 +556,7 @@ function loadBody() {
             <nav class="toc"></nav>
             <div class="main-container">
                 <article class="article">${ document.body.innerHTML }</article>
-                <footer class="article-footer"><p>This is a simple personal site powered by <a href="https://github.com/irisembury">GitHub</a>. I have no association with any other person or organization. For general inquiry, contact irisembury@gmail.com</p></footer>
+                <footer class="article-footer"><p>This is a personal site powered by <a href="https://github.com/irisembury">GitHub</a>. I have no association with any other person or organization. For general inquiry, contact irisembury@gmail.com</p></footer>
             </div>
             <div class="right-spacer"></div>
         </div>
@@ -568,7 +568,6 @@ function loadBody() {
     navbar = document.querySelector(".navbar");
     toc = document.querySelector(".toc");
     interpreter(document.querySelector(".article"));
-    HTML.classList.add("js");
 }
 let canTocUpdate = true, tocLastHeading = 0, rowsInToc = [], pageHeadings = [];
 function tocUpdate() {
@@ -603,9 +602,6 @@ function tocUpdate_() {
     tocLastHeading = currentHeading;
 }
 function setupLightswitch() {
-    if (index) {
-        return;
-    }
     const lightswitch = document.getElementById("lightswitch");
     if (!lightswitch) { console.error("couldn't find #lightswitch"); }
     if (lightswitch) {
